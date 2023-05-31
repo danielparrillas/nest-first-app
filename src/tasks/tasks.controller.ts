0333bd1f2ada31e4c42e9ad7de03b879d16c3b50
Brandon Parrillas <brandonps.dev@gmail.com>
2023-05-31 10:01:42 -0600

    end api

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDTO, UpdateTaskDTO } from './dto/task.dto';
 
 @Controller('tasks')
 export class TasksController {
+  constructor(private taskService: TasksService) {}
+
   @Get()
-  helloworld() {
-    return 'Hello world';
+  getAllTasks() {
+    return this.taskService.getAll();
+  }
+
+  @Post()
+  createTask(@Body() newTask: CreateTaskDTO) {
+    return this.taskService.create({
+      title: newTask.title,
+      description: newTask.description,
+    });
+  }
+
+  @Delete(':id')
+  deleteTask(@Param('id') id: string) {
+    this.taskService.deleteTask(id);
+  }
+
+  @Patch(':id')
+  updateTask(@Param('id') id: string, @Body() fields: UpdateTaskDTO) {
+    return this.taskService.update(id, fields);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.entity';
+import { v4 } from 'uuid';
+import { UpdateTaskDTO, CreateTaskDTO } from './dto/task.dto';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [
+    {
+      id: '1',
+      title: 'first task',
+      description: 'some task',
+      status: TaskStatus.PENDING,
+    },
+  ];
+  getAll() {
+    return this.tasks;
+  }
+  create({ title, description }: CreateTaskDTO) {
+    const task = {
+      id: v4(),
+      title,
+      description,
+      status: TaskStatus.IN_PROGRESS,
+    };
+    this.tasks.push(task);
+
+    return task;
+  }
+  update(id: string, fields: UpdateTaskDTO): Task {
+    const currentTask = this.getById(id);
+    const updatedTask = Object.assign(currentTask, fields);
+    this.tasks = this.tasks.map((task) =>
+      task.id === id ? updatedTask : currentTask,
+    );
+    return updatedTask;
+  }
+  getById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+  deleteTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+}
